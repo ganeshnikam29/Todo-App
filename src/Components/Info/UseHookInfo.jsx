42cce6f7945c27c1b208d3f0b6c05927a46daaa7
Ganesh Nikam <ganeshni@synerzipune.local>
2023-05-07 16:35:46 +0530

    Added info for respective implementation

---
+++ src/Components/Info/UseHookInfo.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const UseHookInfo = () => {
+  return (
+    <div className="info-container">
+      <h3>useState Hook</h3>
+      <p>
+        <ul>
+          <li className="content-list-item">
+            <b>useState</b> is a built-in React Hook that allows you to manage
+            state in a functional component.
+          </li>
+          <li className="content-list-item">
+            <b>useState</b> takes an initial state value as an argument and returns an array
+            containing the current state value and a function to update the
+            state value
+          </li>
+          <li className="content-list-item">
+            <b>useState</b> is suitable for managing simple state values that
+            don't require complex logic or updates.
+          </li>
+        </ul>       
+      </p>
+    </div>
+  );
+};
---
+++ src/Components/Info/useReducerInfo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const UseReducerInfo = () => {
+  return (
+    <div className="info-container">
+      <h3>useReducer Hook</h3>
+      <p>
+        <ul>
+          <li className="content-list-item">
+            <b>useReducer</b> is another built-in React Hook that allows you to manage more complex state 
+            in a functional component.
+          </li>
+          <li className="content-list-item">
+            <b>useReducer</b>  takes a reducer function and an initial state value as arguments 
+            and returns an array containing the current state value and a dispatch function to update the state value.
+          </li>
+          <li className="content-list-item">
+            <b>useReducer</b>  is suitable for managing state that requires complex logic and multiple actions to update.
+          </li>
+        </ul>       
+      </p>
+    </div>
+  );
+};
